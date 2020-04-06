5dbd81312ee720a547db7bff62f76e9be3349e8a
LeoSirius <is.li.xiaoyu@qq.com>
2020-04-06 21:24:11 +0800

    Updates

---
+++ precache-manifest.b6d39894a124e244d0afa0f38099f8e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64566d5a92b9db1c60787ab566f73577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "426fe5a615f20d73d85b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d090accd12170575659",
+    "url": "/static/js/2.f59639fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f59639fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426fe5a615f20d73d85b",
+    "url": "/static/js/main.01f3a8b1.chunk.js"
+  },
+  {
+    "revision": "3f70473541e03fcf58d4",
+    "url": "/static/js/runtime-main.a658f6ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
